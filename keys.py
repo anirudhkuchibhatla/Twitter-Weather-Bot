8b720690bb4e577e0522b47f16ec0883217c458c
anirudhkuchibhatla <36802955+anirudhkuchibhatla@users.noreply.github.com>
2019-12-29 16:13:30 -0600

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,8 @@
+# Keys from Twitter API
+your_consumer_key = 'some random string'
+your_consumer_secret = 'some random string'
+your_access_token = 'some random string'
+your_access_token_secret = 'some random string'
+
+# Key from Open Weather Map
+your_API_key = 'some random string'
